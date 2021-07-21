9033661b6e2d2e942cf8b4325af2269c008700c5
lumsdnb <jeremy@lums.io>
2021-07-21 11:57:25 +0200

    Fix toggle btn text

--- loadScene.js
+++ loadScene.js
@@ -1,10 +1,15 @@
 function addScene() {
-    if (!document.querySelector(".camera-scene")) {
-        var scene = document.createElement("iframe")
-        scene.classList.add("camera-scene")
-        scene.setAttribute("src", "ar.html")
-        scene.setAttribute("height", "100vh")
-        document.querySelector("body").appendChild(scene)
-        document.querySelector(".camera-toggle-button").innerHTML = "<p>kamera schliesen</p>"
-    } else document.querySelector(".camera-scene").remove()
+  if (!document.querySelector('.camera-scene')) {
+    var scene = document.createElement('iframe');
+    scene.classList.add('camera-scene');
+    scene.setAttribute('src', 'ar.html');
+    scene.setAttribute('height', '100vh');
+    document.querySelector('body').appendChild(scene);
+    document.querySelector('.camera-toggle-button').innerHTML =
+      '<p>kamera schliesen</p>';
+  } else {
+    document.querySelector('.camera-scene').remove();
+    document.querySelector('.camera-toggle-button').innerHTML =
+      '<p>Hinweise suchen</p>';
+  }
 }
