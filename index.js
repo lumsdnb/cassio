3e7804cd85bf2b5e4a405035930459fe568183b7
lumsdnb <jeremy@lums.io>
2021-08-02 13:59:43 +0200

    Move assets into own folder, add 7 marker handlers

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require('express');
+// const fs = require('fs');
 const app = express();
 const http = require('http');
+// const options = {
+//   key: fs.readFileSync('key.pem'),
+//   cert: fs.readFileSync('cert.pem'),
+// };
 const server = http.createServer(app);
 const { Server } = require('socket.io');
 const io = new Server(server);
@@ -15,6 +20,7 @@ app.get('/', (req, res) => {
 
 io.on('connection', (socket) => {
   console.log('a user connected');
+  console.log(socket.id);
 });
 
 server.listen(3000, () => {
