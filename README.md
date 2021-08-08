9a6f2c3174449746657e6f6c4c951a4a592766db
lums <jeremyondrey@gmail.com>
2021-08-09 00:43:24 +0200

    Update build

--- README.md
+++
@@ -1,1 +0,0 @@
-# cassio-AR-MVP
--- component.js
+++
@@ -1,9 +0,0 @@
-AFRAME.registerComponent("load-camera", {
-
-  init: function () {
-    console.log("load camera");
-    var data = this.data;
-    var el = this.el;
-    console.table(data, el);
-  },
-});
