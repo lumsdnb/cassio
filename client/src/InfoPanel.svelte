ec89b9976cdbb0a0aa4d9c7f35514518fbf9e469
lums <jeremyondrey@gmail.com>
2021-08-10 20:00:07 +0200

    Improved layout

--- client/src/InfoPanel.svelte
+++ client/src/InfoPanel.svelte
@@ -1,16 +1,17 @@
 <script>
     export let itemName
     export let itemDescription
+    export let src
 </script>
 <section class="collectible-info" >
   <button on:click>X</button>
-    <h2 id="collectible-info-title">item {itemName}</h2>
-    <div class="circle">yo</div>
+    <h2 id="collectible-info-title">{itemName}</h2>
+    <!-- <div class="circle">yo</div> -->
     <article>
       <img
         class="collectible-info-image"
-        src="https://source.unsplash.com/random/200×200"
-        alt="imag"
+        {src}
+        alt={`${itemName} image`}
       />
       <p id="collectible-info-text">{itemDescription}</p>
     </article>
@@ -19,8 +20,9 @@
 <style>
   .collectible-info{
     background-color: var(--white);
-    margin: 1rem;
+    margin: 0;
     padding: 1rem;
+    max-width: 100%;
     border-radius: 0.15rem;
     filter: drop-shadow(3px 3px 5px rgba(0, 0, 0, 0.419));
     overflow: scroll;
@@ -29,6 +31,16 @@
     position: absolute;
     right: 1rem;
     top: 1rem;
-    padding: 2rem;
+    font-size: 1.5rem;
+    padding: 1rem;
   }
+
+  .collectible-info>article>p::first-letter {
+  font-size: 350%;
+  font-family: 'eckmann';
+  color: #333333;
+}
+.collectible-info>article{
+  max-width: 90vw;
+}
 </style>
